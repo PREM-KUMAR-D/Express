d105ab4bc1f4ddb895fe1438fe86155d1f996938
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-06-01 12:50:38 +0530

    Added routing and a form to accept product size and product name

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const http  = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("In the middleware!");
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product', (req, res, next) => {
+    
+    res.send('<form action="/product" method ="POST" >Product:<input type="text" name="title"> Size: <input type="number" name="size"> <button type="submit"> Add Product</button> </form> ');
+    // next();
 });
 
-app.use((req,res,next)=>{
-    console.log("In the next middleware!");
-    res.send({num:1});
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+    
 });
+
+
+app.use('/', (req, res, next) => {
+    
+    res.send({ num: 1 });
+    
+});
+
+
 
 app.listen(4000);
 
